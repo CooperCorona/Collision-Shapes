27659525d08603dff14f440e47983249161982f7
CooperCorona <cooperk@plvwtelco.net>
2015-09-26 16:41:16 -0500

    Initial Commit

---
+++ CollisionShapesTests/CollisionShapesTests.swift
@@ -0,0 +1,36 @@
+//
+//  CollisionShapesTests.swift
+//  CollisionShapesTests
+//
+//  Created by Cooper Knaak on 9/26/15.
+//  Copyright © 2015 Cooper Knaak. All rights reserved.
+//
+
+import XCTest
+@testable import CollisionShapes
+
+class CollisionShapesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
